80deeb931da5b886554e3d0f3d4a7dc3527c828d
joel-tene <joel.tene@unl.edu.ec>
2026-06-12 13:11:09 -0500

    Javascript

---
+++ Unidad2/00Javascript/string.js
@@ -0,0 +1,17 @@
+let string1= 'Hola mundo'
+let string2= "Javascript es genial"
+let string3= `${string1} feliz:)`
+let string4= string1 + ' ' + string2
+
+console.log(string1)
+console.log(string2)
+console.log(string3)
+console.log(string4)
+
+let frase = 'JavaScript es genial'
+console.log(frase.length) 
+console.log(frase.toUpperCase())
+console.log(frase.toLowerCase()) 
+console.log(frase.toLocaleLowerCase()) 
+console.log(frase.toLowerCase()) 
+console.log(frase.substring(0, 12)) 
---
+++ Unidad2/00Javascript/tipos.datos.js
@@ -0,0 +1,37 @@
+//Primitivos
+let=nombre=Joel;//String
+
+//Numericos
+let edad= 19;//Number
+
+//Booleanos
+let esMayorDeEdad=true;//Boolean
+
+//Null
+let noHayValor=null;//Null
+
+//Undefined
+let noDefinido= undefined;//Undefined
+
+//Symbol VALOR UNICO
+let simboloUnico=Symbol('simbolo');
+
+//DATOS GIGANTES BIGINT
+let numeroGrande= 2n;
+
+//Complejos
+//object
+let carro={
+    marca:'Toyota',
+    modelo:'Corolla',
+    año:2020
+};
+
+//Arrays
+
+let frutas =['Manzana','Banana','Naranja'];
+
+//Funciones
+function saludar(){}
+
+
