932a2598db41a3e4e03e0b01ae87639da00ac42e
joel-tene <joel.tene@unl.edu.ec>
2026-06-16 11:42:39 -0500

    16dejunio2026

---
+++ Unidad2/clase-conversion.js
@@ -0,0 +1,14 @@
+const string = '47'
+const integer = parseInt(string)
+console.log(typeof integer)
+
+const string1 = '3.14'
+const float = parseFloat(string1)
+console.log(float)
+
+const binary = '10010'
+const decimal = parseInt(binary, 2)
+console.log(decimal)
+
+const suma =  3 - true
+console.log(suma)
---
+++ Unidad2/clase.number.js
@@ -0,0 +1,38 @@
+//Tipos de datos numéricos
+// entero y decimal
+
+const entero = 42
+const decimal = 3.14
+console.log(typeof entero, typeof decimal)
+
+//Notación científica
+const científico = 5e3
+
+//Representación de otros contextos.
+const infinito = Infinity
+const noEsUnNumero = NaN
+
+//Operaciones Aritmeticas
+
+//Sumas Restas
+
+const suma = 3+4
+const resta = 4-4
+const multiplicacion = 4*7
+const division = 10/2
+
+//Modulos de exponenciación
+const modulo = 15 % 8
+
+//Resultado de presicion
+const exponenciacion = 0.1 + 0.2
+console.log(exponenciacion)
+console.log(exponenciacion.toFixed(1))
+
+const raizcuadrada = Math.sqrt(16)
+const valorAbsoluto = Math.abs(-7)
+const aleatorio = Math.random()
+console.log(raizcuadrada)
+console.log(valorAbsoluto)
+console.log(aleatorio)
+
